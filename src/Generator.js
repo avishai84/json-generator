5bdb1618c098a82c33c89ead5d1c186c6225f33f
avishai <avishai_taashur@gap.com>
2019-03-04 13:45:21 -0800

    adding generate json btn

--- src/Generator.js
+++ src/Generator.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
-
 import './Generator.scss';
-
+let increament = 0;
 
 class Generator extends Component {
 
@@ -16,6 +15,7 @@ class Generator extends Component {
         this.handleChange = this.handleChange.bind(this);
         this.generateInputFromForm =  this.generateInputFromForm.bind(this);
         this.originalValueChanged = this.originalValueChanged.bind(this);
+        this.editFormBtn = this.editFormBtn.bind(this);
     }
     
 
@@ -33,10 +33,19 @@ class Generator extends Component {
           // and change bg color for visual indication
 
         this.setState({
-            valueChange: console.log(event.target)
-            //(event.target.value !== "" && event.target.attributes['placeholder'].nodeValue !== event.target.value) ? event.target.setAttribute('style','background-color:#0092f61f') : event.target.setAttribute('style','background-color:transparent')
+            valueChange: (event.target.value !== "" && event.target.attributes['placeholder'].nodeValue !== event.target.value) ? (event.target.setAttribute('style','background-color:#dc35454a'), this.editFormBtn('true')) : event.target.setAttribute('style','background-color:transparent')
         });
       }
+
+      editFormBtn(canEdit){
+        
+        if(canEdit === 'true'){
+            console.log('CAN EDIT - add btn for saving edited json')
+        this.setState({
+            createBtn : document.querySelector('.generator-wrapper').contains(document.querySelector('.generateForm')) ? 'false' : document.querySelector('.flex-wrapper > section:nth-child(1)').insertAdjacentHTML('afterbegin','<button class="btn btn-warning generateForm">Generate JSON</button>')
+            })
+            }
+        }
 
     generateInputFromForm(event){
        
@@ -49,7 +58,7 @@ class Generator extends Component {
           // test if string looks like an object
             const regexStartWith = /^{/gm;
             const regexEndWith = /}$/gm;
-            let itemsBuilder = '';
+            let itemsBuilder = ' <form class="theNewForm" name="myForm" >';
             if(regexStartWith.test(jsonRaw.value) && regexEndWith.test(jsonRaw.value)){
                 let formdata = JSON.parse(jsonRaw.value); 
                 // test for a real object
@@ -57,32 +66,26 @@ class Generator extends Component {
                     // loop though the object and make it editable
                     
                     for(let key in formdata){
-         
-                       // console.table(key, formdata[key]);
-                     
-                    
 
-                        itemsBuilder += `<div><label for=${key}>${key}<input class="form-control" id=${key} type="text" placeholder=${formdata[key]} /></label></div>`;
+                        increament = increament+1;
 
-                  
-                        
-                        //console.table( itemsBuilder);
-
+                        itemsBuilder += `<div><label for=${key}${increament}>${key}<input class="form-control" id=${key}${increament} type="text" placeholder=${formdata[key]} /></label></div>`;
 
                         // level 2
-                        // let formdata2 = formdata;
-                        // if(key === "data"){
-                        //     for(let key2 in formdata2[key]){
-                        //         console.table(key2, formdata2[key][key2]);
-                        //     }
-                        // };
-                            // for(let key2 in formdata2[key]){
-                            //     console.log(formdata2[key][key2]);
-                            // }
+                         let formdata2 = formdata;
+                            if(key === "data"){
+                                for(let key2 in formdata2[key]){
+                                    itemsBuilder += `<div><label for=${key2}>${key2}<input class="form-control" id=${key2} type="text" placeholder=${formdata2[key][key2]} /></label></div>`;
+                                    console.table(key2, formdata2[key][key2]);
+                                }
+                            }
                         }
+                        itemsBuilder += '</form>';
                         // after state after loop end
                         this.setState({
-                            populateForm: document.querySelector('.editableForm').innerHTML = itemsBuilder
+                            populateForm: document.querySelector('.editableForm').innerHTML = itemsBuilder,
+                            c: document.querySelectorAll('.editableForm .theNewForm input').forEach( (item) => 
+                                item.addEventListener('blur', this.originalValueChanged))
                         });
                    // console.log('lets do something with this json ');
                     }
@@ -93,7 +96,6 @@ class Generator extends Component {
             }
         }
     }
-
 
 
     componentDidMount(){
@@ -112,7 +114,7 @@ class Generator extends Component {
                     <label htmlFor="jsonRaw">Enter json:</label>
                     <textarea id="jsonRaw" rows="10" cols="30" placeholder="Paste JSON file in here" onChange={this.handleChange} ></textarea>
                 </div>
-                 <input type="submit" onClick={this.generateInputFromForm} className="btn btn-primary" value="Generate"/> 
+                 <input type="submit" onClick={this.generateInputFromForm} className="btn btn-primary" value="Edit JSON"/> 
             </form>
         </main>
         <div className="flex-wrapper">
